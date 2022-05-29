a6a238223887be7b4f7c68605bee04f977ff096e
Idris AY <97283155+idrisay@users.noreply.github.com>
2022-05-29 18:59:06 +0200

    feat: add footer and header layouts

---
+++ components/navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from '../styles/Navbar.module.css';
+
+const navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+        <h1>
+            Navbar
+        </h1>
+    </nav>
+  )
+}
+
+export default navbar
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import 'antd/dist/antd.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+import Layout from '../components/layout'
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
-export default MyApp
+
